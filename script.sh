178963404eff19a5e23362d8c05d0825e139bd02
Juan Luis Mejía Villa <jlmejiav@unal.edu.co>
2019-05-12 16:52:27 -0500

    Seg

--- script.sh
+++ script.sh
@@ -3,4 +3,4 @@ cat $(echo estaciones/$(head -n 1 ListNombres.txt)) > out1.csv
 csvstack -g $(echo $(cat ListNombres.txt) | tr ' ' ',') -n Estacion $(echo out1.csv $(printf 'estaciones/%s ' $(tail +2 ListNombres.txt))) > consolidado.csv
 sed 's/,/;/' consolidado.csv | tr ',' '.' | tr ';' ',' > out2.csv
 sudo apt-get install -y csvkit
-csvsql --query 'select Estacion,FECHA,VEL from out2 group by Estacion' out2.csv > out3.csv
+csvsql --query 'select Estacion,FECHA,VEL from out2.csv group by Estacion' out2.csv > out3.csv
