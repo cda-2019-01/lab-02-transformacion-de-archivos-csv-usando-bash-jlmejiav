de2d42a570be6aaa7a2fb9adfa1a3e0c5674e82b
Juan Luis Mejía Villa <jlmejiav@unal.edu.co>
2019-05-11 19:08:50 -0500

    Ready la unión de archivos automático
    No depende de la cantidad de archivos ni el nombre del archivo, lo hace automático y no repite los headers. Lo único es que tiene las rutas respecto a lab-02, se podría poner las rutas (paths) desde la raiz.

--- script.1.sh
+++ script.1.sh
@@ -1,4 +1,4 @@
 ls estaciones > ListNombres.txt
-cat $(echo estaciones/$(head -n 1 ListNombres.txt)) > consolidado.csv
-csvstack -g $(echo $(cat ListNombres.txt) | tr ' ' ',') -n Estacion consolidado.csv $(printf 'estaciones/%s ' $(tail +2 ListNombres.txt)) > consolidado.csv
-sed 's/\(E[0-9]\),/\1;/' consolidado.csv | tr ',' '.' | tr ';' ',' > out.1
+cat $(echo estaciones/$(head -n 1 ListNombres.txt)) > out1.csv
+csvstack -g $(echo $(cat ListNombres.txt) | tr ' ' ',') -n Estacion $(echo out1.csv $(printf 'estaciones/%s ' $(tail +2 ListNombres.txt))) > consolidado.csv
+sed 's/,/;/' consolidado.csv | tr ',' '.' | tr ';' ',' > out2.csv
--- script.sh
+++ script.sh
@@ -1,3 +1,3 @@
 ls estaciones > ListNombres.txt
 csvstack -g $(echo $(cat ListNombres.txt) | tr ' ' ',') -n Estacion $(printf 'estaciones/%s ' $(cat ListNombres.txt)) > consolidado.csv
-sed 's/\(E[0-9]\),/\1;/' consolidado.csv | tr ',' '.' | tr ';' ',' > out.1
+sed 's/,/;/' consolidado.csv | tr ',' '.' | tr ';' ',' > out.1
